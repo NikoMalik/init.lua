da6b1a00810999ac544a8858cf91f0bedf2f0045
NikoMalik <NikoMalik@users.noreply.github.com>
2025-10-09 23:52:35 +0300

    obisidian view

--- init.lua
+++ init.lua
@@ -30,6 +30,21 @@ vim.keymap.set({ 'n', 'i' }, '<C-s>', '<cmd>w<CR>', { desc = 'General save file'
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+vim.api.nvim_create_user_command('JournalToday', function()
+  local today = os.date '%Y-%m-%d'
+  local path = string.format('~/notes/%s.md', today)
+  vim.cmd('edit ' .. path)
+end, {})
+vim.keymap.set('n', '<leader>jn', ':JournalToday<CR>', { desc = 'Open today’s journal' })
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  callback = function(opts)
+    if vim.bo[opts.buf].filetype == 'markdown' then
+      vim.opt.conceallevel = 2
+    end
+  end,
+})
+
 vim.keymap.set('n', '\\', '<cmd>:vsplit <CR>', { desc = 'Vertical Split' })
 vim.keymap.set('n', '|', ':split<CR>', { desc = 'Horizontal Split' })
 -- vim.cmd.colorscheme 'habamax' -- super great but a litle bit smoke
@@ -123,6 +138,7 @@ vim.o.termguicolors = true
 vim.o.showmode = false
 
 vim.opt.swapfile = false
+vim.cmd 'setlocal conceallevel=2'
 vim.o.list = false
 
 vim.schedule(function()
@@ -259,16 +275,23 @@ require('lazy').setup({
   },
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
-  {
-    'OXY2DEV/markview.nvim',
-    lazy = false,
+  -- {
+  --   'OXY2DEV/markview.nvim',
+  --   lazy = false,
+  --   priority = 49, -- default - 1
+  --   dependencies = {
+  --     'nvim-treesitter/nvim-treesitter',
+  --     'nvim-tree/nvim-web-devicons',
+  --   },
+  --   opts = {
+  --     headings = {
+  --       heading_1 = { sign = '' },
+  --       heading_2 = { sign = '' },
+  --     },
+  --     code_blocks = { sign = false },
+  --   },
+  -- },
 
-    -- For blink.cmp's completion
-    -- source
-    dependencies = {
-      'saghen/blink.cmp',
-    },
-  },
   {
     'nvim-neo-tree/neo-tree.nvim',
     version = 'v3.x',
@@ -422,6 +445,111 @@ require('lazy').setup({
       },
     },
   },
+
+  {
+    'epwalsh/obsidian.nvim',
+    version = '*',
+    lazy = true,
+    ft = 'markdown',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      workspaces = {
+        {
+          name = 'notes',
+          path = '~/notes/',
+        },
+      },
+
+      {
+        name = 'no-vault',
+        path = function()
+          -- alternatively use the CWD:
+          -- return assert(vim.fn.getcwd())
+          return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+        end,
+        overrides = {
+          notes_subdir = vim.NIL, -- have to use 'vim.NIL' instead of 'nil'
+          new_notes_location = 'current_dir',
+          templates = {
+            folder = vim.NIL,
+          },
+          disable_frontmatter = true,
+        },
+      },
+
+      ui = {
+        enable = true, -- set to false to disable all additional syntax features
+        update_debounce = 200, -- update delay after a text change (in milliseconds)
+        max_file_length = 5000, -- disable UI features for files with more than this many lines
+        -- Define how various check-boxes are displayed
+        checkboxes = {
+          -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+          [' '] = { char = '󰄱', hl_group = 'ObsidianTodo' },
+          ['x'] = { char = '', hl_group = 'ObsidianDone' },
+          ['>'] = { char = '', hl_group = 'ObsidianRightArrow' },
+          ['~'] = { char = '󰰱', hl_group = 'ObsidianTilde' },
+          ['!'] = { char = '', hl_group = 'ObsidianImportant' },
+          -- Replace the above with this if you don't have a patched font:
+          -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+          -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+          -- You can also add more custom ones...
+        },
+
+        -- Use bullet marks for non-checkbox lists.
+        bullets = { char = '•', hl_group = 'ObsidianBullet' },
+        external_link_icon = { char = '', hl_group = 'ObsidianExtLinkIcon' },
+        -- Replace the above with this if you don't have a patched font:
+        -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+        reference_text = { hl_group = 'ObsidianRefText' },
+        highlight_text = { hl_group = 'ObsidianHighlightText' },
+        tags = { hl_group = 'ObsidianTag' },
+        block_ids = { hl_group = 'ObsidianBlockID' },
+        hl_groups = {
+          -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+          ObsidianTodo = { bold = true, fg = '#f78c6c' },
+          ObsidianDone = { bold = true, fg = '#89ddff' },
+          ObsidianRightArrow = { bold = true, fg = '#f78c6c' },
+          ObsidianTilde = { bold = true, fg = '#ff5370' },
+          ObsidianImportant = { bold = true, fg = '#d73128' },
+          ObsidianBullet = { bold = true, fg = '#89ddff' },
+          ObsidianRefText = { underline = true, fg = '#c792ea' },
+          ObsidianExtLinkIcon = { fg = '#c792ea' },
+          ObsidianTag = { italic = true, fg = '#89ddff' },
+          ObsidianBlockID = { italic = true, fg = '#89ddff' },
+          ObsidianHighlightText = { bg = '#75662e' },
+        },
+      },
+
+      picker = {
+        -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+        name = 'telescope.nvim',
+        -- Optional, configure key mappings for the picker. These are the defaults.
+        -- Not all pickers support all mappings.
+        note_mappings = {
+          -- Create a new note from your query.
+          new = '<C-x>',
+          -- Insert a link to the selected note.
+          insert_link = '<C-l>',
+        },
+        tag_mappings = {
+          -- Add tag(s) to current note.
+          tag_note = '<C-x>',
+          -- Insert a tag at the current location.
+          insert_tag = '<C-l>',
+        },
+      },
+      mappings = {},
+      completion = {
+        nvim_cmp = false,
+      },
+      templates = {
+        folder = 'templates',
+        date_format = '%Y-%m-%d',
+      },
+    },
+  },
+
   {
     'nvim-telescope/telescope-file-browser.nvim',
     dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
@@ -621,6 +749,10 @@ require('lazy').setup({
             enable_build_on_save = true,
             semantic_tokens = 'partial',
           },
+        },
+        nix = {
+          cmd = { 'nixd' },
+          filetypes = { 'nix' },
         },
         clangd = {
           cmd = {
