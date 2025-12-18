7228fc901cfce74a89b4272f2aa423536bc8a6be
NikoMalik <NikoMalik@users.noreply.github.com>
2025-12-18 19:01:29 +0300

    fix: delete dgraph,git has nice builtin graphs

--- init.lua
+++ init.lua
@@ -241,7 +241,7 @@ vim.o.termguicolors = true
 vim.o.showmode = false
 
 vim.opt.swapfile = false
-vim.cmd 'setlocal conceallevel=0'
+-- vim.cmd 'setlocal conceallevel=0'
 vim.o.list = false
 
 vim.schedule(function()
@@ -409,37 +409,6 @@ require('lazy').setup({
         -- there are no required options atm
       }
     end,
-  },
-  {
-    'isakbm/gitgraph.nvim',
-    opts = {
-      git_cmd = 'git',
-      symbols = {
-        merge_commit = 'M',
-        commit = '*',
-      },
-      format = {
-        timestamp = '%H:%M:%S %d-%m-%Y',
-        fields = { 'hash', 'timestamp', 'author', 'branch_name', 'tag' },
-      },
-      hooks = {
-        on_select_commit = function(commit)
-          print('selected commit:', commit.hash)
-        end,
-        on_select_range_commit = function(from, to)
-          print('selected range:', from.hash, to.hash)
-        end,
-      },
-    },
-    keys = {
-      {
-        '<leader>gl',
-        function()
-          require('gitgraph').draw({}, { all = true, max_count = 5000 })
-        end,
-        desc = 'GitGraph - Draw',
-      },
-    },
   },
 
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
