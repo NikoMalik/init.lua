e4dcdcbad709dc2113c7d68ad1f766146d0d7806
NikoMalik <NikoMalik@users.noreply.github.com>
2026-03-07 22:28:24 +0300

    feat: venn

--- init.lua
+++ init.lua
@@ -471,7 +471,41 @@ require('lazy').setup({
 				}
 			end,
 		},
+		{
+			'jbyuki/venn.nvim',
+			keys = {
+				{
+					'<leader>vv',
+					function()
+						local venn_enabled = vim.inspect(vim.b.venn_enabled)
+						if venn_enabled == "nil" then
+							vim.b.venn_enabled = true
+							vim.opt_local.virtualedit = "all"
+							vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", { noremap = true })
+							vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", { noremap = true })
+							vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", { noremap = true })
+							vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", { noremap = true })
+							vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", { noremap = true })
+							print("Venn ON")
+						else
+							vim.b.venn_enabled = nil
+							vim.opt_local.virtualedit = ""
+							vim.api.nvim_buf_del_keymap(0, "n", "J")
+							vim.api.nvim_buf_del_keymap(0, "n", "K")
+							vim.api.nvim_buf_del_keymap(0, "n", "L")
+							vim.api.nvim_buf_del_keymap(0, "n", "H")
+							vim.api.nvim_buf_del_keymap(0, "v", "f")
+							print("Venn OFF")
+						end
+					end,
+					desc = 'Toggle Venn',
+				},
+			},
+		},
 
+		-- <leader>vv - enable/disable. in venn:
+		-- - HJKL - draw lines (─ │ ┌ ┐ └ ┘)
+		-- - visual select + f — draw in box (┌──┐ │ │ └──┘)
 		'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 		{
 			'nvim-tree/nvim-tree.lua',
