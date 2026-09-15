ee1ca034a4dde2f67b66e0e70fe3b4feb05ff1ee
NikoMalik <NikoMalik@users.noreply.github.com>
2026-09-15 22:13:10 +0300

    fix: disable lsp colors

--- init.lua
+++ init.lua
@@ -73,6 +73,9 @@ vim.api.nvim_create_autocmd('FileType', {
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client then
+			client.server_capabilities.semanticTokensProvider = nil
+		end
 		if client and client.name == "iwes" then
 			client.server_capabilities.documentFormattingProvider = false
 			client.server_capabilities.documentRangeFormattingProvider = false
